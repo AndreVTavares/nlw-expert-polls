b856f80431a37c6fa7403648b42716a073d3918b
André Tavares <andretavares3@gmail.com>
2024-03-12 11:35:13 -0300

    new route vote on polls

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,12 +1,14 @@
 import fastify from 'fastify'
 import { CreatePoll } from './routes/create-poll'
 import { GetPoll } from './routes/get-poll'
+import { VoteOnPoll } from './routes/vote-on-poll'
 
 
 const app = fastify()
 
 app.register(CreatePoll)
 app.register(GetPoll)
+app.register(VoteOnPoll)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running')
