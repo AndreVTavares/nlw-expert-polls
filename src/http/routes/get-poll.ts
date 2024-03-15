0cdb1cb7e271ee2cc388da16e0e57504d95fbd4d
André Tavares <andretavares3@gmail.com>
2024-03-14 21:03:35 -0300

    better way to show the scores when using get poll route

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -41,7 +41,19 @@ export async function GetPoll(app: FastifyInstance) {
             return obj
         }, {} as Record<string, number>)
 
-        return reply.send({ votes, poll })
+        return reply.send({
+            poll: {
+                id: poll.id,
+                title: poll.title,
+                options: poll.options.map(option => {
+                    return {
+                        id: option.id,
+                        title: option.title,
+                        score: (option.id in votes) ? votes[option.id] : 0,
+                    }
+                })
+            }
+        })
 
     })
 }
