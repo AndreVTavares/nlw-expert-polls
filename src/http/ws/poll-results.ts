c755b2858ba73ab2d94783ca66bd4e82461050d4
André Tavares <andretavares3@gmail.com>
2024-03-17 20:11:16 -0300

    creating utils voting-pub-sub to use websockets

--- src/http/ws/poll-results.ts
+++ src/http/ws/poll-results.ts
@@ -3,7 +3,6 @@ import { FastifyInstance } from "fastify";
 export async function pollResults(app: FastifyInstance) {
     app.get('/polls/:pollId/results', { websocket: true }, (connection, request) => {
         connection.socket.on('message', (message: string) => {
-            // message.toString() === 'hi from client'
             connection.socket.send('You sent' + message)
         })
     })
